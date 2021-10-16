c9fef4fe886a5d64a7ad7e6b55125c8d6515c25b
epsilonlyra <kozlov.de@phystech.edu>
2021-10-16 23:06:01 +0300

    time and incognito mode

--- ball_click/balls.py
+++ ball_click/balls.py
@@ -15,6 +15,69 @@ else returns -1
   else:
     return(-1)
 
+
+
+#  if leaderboard file  doesnt exist, creates it
+try:
+    BESTPLAYERS = open('BESTPLAYERS.txt', 'x')
+    BESTPLAYERS.close()
+    already_exists = False
+except:
+    already_exists = True
+
+
+BESTPLAYERS = open('BESTPLAYERS.txt', 'r')
+#  list of strings: 'position, name, score, time'
+AllPlayers = BESTPLAYERS.readlines()  
+BESTPLAYERS.close()
+
+Players=[]  #  list of lists of strings: 'position' 'name' 'score' 'time'
+for player in AllPlayers:
+    Players.append(player.split())
+
+# list of lists [score, name, time]
+# time and name are not activly used
+ScoreNames=[] 
+for player in Players:
+    player[2] = int(player[2])  # score to int
+    ScoreNames.append([player[2], player[1], player[3]])
+
+
+print('What is Your Name, oh  New Great Hero?')
+print('Press "Enter" if You Dont Want to Have Your Score on Leaderbord')
+
+# checking if given name is possible
+name_good = False
+while not name_good:
+    NAME = input('NAME:')  
+    if NAME == '':
+        blank_name = True  # incognito mode activation
+    else:
+        blank_name = False
+
+        overlap_names = False
+        for scorename in ScoreNames:
+            if (NAME != scorename[1]):
+                continue
+            overlap_names = True
+        
+        spaces_in_name = False    
+        if len(NAME.split()) != 1:
+            spaces_in_name = True
+            
+    if  not blank_name: 
+        if (not spaces_in_name) and (not overlap_names):
+            name_good = True
+        elif (spaces_in_name):
+            print("Do not Use Spaces in Name")
+        elif (overlap_names):
+            print("Name  already Taken")
+    else:
+      name_good = True
+      print('Aнонимность Инкогнито Неузнаваемая Личность')
+      print('Incognito mode activated\nYour Score Will Not be Saved')
+
+
 pygame.init()
 
 FPS = 30
@@ -34,68 +97,18 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-
-try:
-    BESTPLAYERS = open('BESTPLAYERS.txt', 'x')
-    BESTPLAYERS.close()
-    already_exists = False
-except:
-    already_exists = True
-
-BESTPLAYERS = open('BESTPLAYERS.txt', 'r')
-AllPlayers = BESTPLAYERS.readlines()
-BESTPLAYERS.close()
-
-Players=[]
-for player in AllPlayers:
-    Players.append(player.split())
-
-
-ScoreNames=[]    
-for player in Players:
-    player[2] = int(player[2])
-    ScoreNames.append([player[2], player[1]])
-    print(ScoreNames)
-
-
-print('What is Your Name, oh Great Hero?')
-
-if already_exists:
-    name_good = False
-else:
-    name_good = True
-    NAME = input('NAME:')
-
-while not name_good:
-    NAME = input('NAME:')
-                 
-    overlap_names = False
-    for scorename in ScoreNames:
-        if (NAME != scorename[1]):
-            continue
-        overlap_names = True
-
-    spaces_in_name = False
-    if len(NAME.split()) != 1:
-        spaces_in_name = True
-
-    if (not spaces_in_name) and (not overlap_names):
-        name_good = True
-    elif (spaces_in_name):
-        print("Do not Use Spaces in Name")
-    elif (overlap_names):
-        print("Name  already Taken")
-
         
 class Ball():
      
     def __init__(self):
-        
+      
+        self.GEN_RATE = 1000 #  time in ms beetwen ball generations
+        self.MAX_AMOUNT = 10  #  maximum amount of ball on screen any time
         self.x = randint(100, width - 100)
         self.y = randint(100, height - 100)
         self.r = randint(30, 50)
         self.color = COLORS[randint(0, 5)]
-        self.speed = 5  #  max module of velocity in x and y direction
+        self.speed = 5 # abs of movement in unit time in x or y direction
         self.v_x = randint(-self.speed, self.speed)
         self.v_y = randint(-self.speed, self.speed)
         
@@ -146,35 +159,36 @@ change velocity randomly, so it doesnt hit it
 
 
 SCORE = 0
-  #  to give it to pyfont we use:
+#  to give it to pyfont we use:
 SCORESIGN = bytes('Score:' + str(SCORE), encoding = 'utf-8')
 
 Balls = []
-GEN_RATE = 1000  #  time in ms beetwen ball generations
-MAX_AMOUNT = 10  #  maximum amount of ball on screen any time
 GENERATE_BALL = pygame.USEREVENT + 0
-pygame.time.set_timer(GENERATE_BALL, GEN_RATE)
+pygame.time.set_timer(GENERATE_BALL, Ball().GEN_RATE)
 
 pygame.display.update()
 clock = pygame.time.Clock()
+
+TIME = 0
 finished = False
 
+START = pygame.time.get_ticks()
 while not finished:
     clock.tick(FPS)
-        
+    TIME += clock.get_time()   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
             
-        if event.type == GENERATE_BALL and len(Balls) != MAX_AMOUNT:
+        if event.type == GENERATE_BALL and len(Balls) != Ball().MAX_AMOUNT:
             Balls.append(Ball())
                                              
         if event.type == pygame.MOUSEBUTTONDOWN:
-              #  check if mouse click is inside the ball
+            #  check if mouse click is inside the ball
             Mouse_coordinates = (pygame.mouse.get_pos())  
             for ball in Balls: 
                 if ((Mouse_coordinates[0] - ball.x) **2 \
-                    + (Mouse_coordinates[1] - ball.y) **2) \
+                      + (Mouse_coordinates[1] - ball.y) **2) \
                     <= (ball.r) **2:
                         Balls.remove(ball)
                         SCORE = SCORE + 1
@@ -183,11 +197,11 @@ while not finished:
                                                                             
     for ball in Balls:
         ball.bounce_wall()
-                            
+        
     for ball in Balls:
         ball.move_x()
         ball.move_y()
-
+        
     for ball  in Balls:
         ball.create()
         
@@ -195,22 +209,18 @@ while not finished:
     screen.blit(img, (20, 20))
                                                                   
     pygame.display.update()
-
     screen.fill(BLACK)
 
 pygame.quit()
 
 
-ScoreNames.append([SCORE, NAME])
-
-def custom_key(ScoreNames):
-    return(ScoreNames[0])
-
-ScoreNames.sort(reverse = True, key = custom_key)
+TIME_PAS =  str(round(TIME/1000)) + 's'
+if not blank_name:
+    ScoreNames.append([SCORE, NAME, TIME_PAS])  #  adding current player
+    ScoreNames.sort(reverse = True)  
 
 BESTPLAYERS = open('BESTPLAYERS.txt', 'w')
-
 for i in range (len(ScoreNames)):
-    print(i + 1, ScoreNames[i][1], ScoreNames[i][0], file  =  BESTPLAYERS)
-
+    print(i + 1, ScoreNames[i][1], ScoreNames[i][0], ScoreNames[i][2],
+          file  =  BESTPLAYERS)
 BESTPLAYERS.close()
