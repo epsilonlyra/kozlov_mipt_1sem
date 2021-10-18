0639dc796d3bae5c1d5e8367f5a04871bd6627db
epsilonlyra <kozlov.de@phystech.edu>
2021-10-19 00:40:14 +0300

    added additional screen and simplified collision with walls

--- ball_click/balls.py
+++ ball_click/balls.py
@@ -31,13 +31,13 @@ BESTPLAYERS = open('BESTPLAYERS.txt', 'r')
 AllPlayers = BESTPLAYERS.readlines()  
 BESTPLAYERS.close()
 
-Players=[]  #  list of lists of strings: 'position' 'name' 'score' 'time'
+Players = []  #  list of lists of strings: 'position' 'name' 'score' 'time'
 for player in AllPlayers:
     Players.append(player.split())
 
 # list of lists [score, name, time]
 # time and name are not activly used
-ScoreNames=[] 
+ScoreNames = [] 
 for player in Players:
     player[2] = int(player[2])  # score to int
     ScoreNames.append([player[2], player[1], player[3]])
@@ -87,6 +87,9 @@ screen = pygame.display.set_mode((width, height))
 
 font = pygame.font.SysFont(None, 24)
 
+ANON_surf= pygame.image.load('anon.bmp')
+ANON_surf =  body_surf = pygame.transform.scale(ANON_surf, (width, height))
+
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -102,11 +105,10 @@ class Ball():
      
     def __init__(self):
       
-        self.GEN_RATE = 1000 #  time in ms beetwen ball generations
-        self.MAX_AMOUNT = 10  #  maximum amount of ball on screen any time
+        self.GEN_RATE = 1000  #  time in ms beetwen ball generations
         self.color = COLORS[randint(0, 5)]
         self.value = 1
-        self.speed = 10 # abs of movement in unit time in x or y direction
+        self.speed = 10  # abs of  maxmovement unit tine in x or y direction
         self.r = randint(30, 50)
         
         self.x = randint(100, width - 100)
@@ -147,7 +149,13 @@ class Ball():
             self.v_x = randint(-min(self.speed, self.x + self.r),
                                min(self.speed, width - self.r - self.x))
 
-    def check_click(self, mouse_coordinates, SCORE):
+    def check_click(self, Mouse_coordinates, SCORE):
+        """
+  checks if mouse in hitbox, and if hit adds value to SCORE
+  returns list
+  list [0] = 1 if hit, 0 if not
+  list[1] - SCORE after click
+      """
         if ((Mouse_coordinates[0] - self.x) **2 \
             + (Mouse_coordinates[1] - self.y) **2) \
                     <= (self.r) **2:
@@ -164,13 +172,12 @@ class Rect():
      
     def __init__(self):
       
-        self.GEN_RATE = 1000 #  time in ms beetwen sqr generations
-        self.MAX_AMOUNT = 10  #  maximum amount of sqr on screen any time
+        self.GEN_RATE = 1700 #  time in ms beetwen sqr generations
         self.color = COLORS[randint(0, 5)]
         self.value = 1
         self.speed = 5 # abs of movement in unit time in x or y direction
-        self.a = randint(30, 50)
-        self.b = randint(30 ,50)
+        self.a = randint(60, 100)  # rect width
+        self.b = randint(60 ,100)  #  rect height
         
         self.x = randint(100, width - 100)
         self.y = randint(100, height - 100)
@@ -210,9 +217,15 @@ class Rect():
                                min(abs(self.speed), width - self.a - self.x))
 
     def check_click(self, mouse_coordinates, SCORE):
+        """
+  checks if mouse in hitbox, and if hit adds value to SCORE
+  returns list
+  list [0] = 1 if hit, 0 if not
+  list[1] - SCORE after click
+        """
         if (mouse_coordinates[0] - self.x >=0) and \
         (mouse_coordinates[0] - self.x <= self.a) and \
-        (mouse_coordinates[1] - self.y >=0) and \
+        (mouse_coordinates[1] - self.y >= 0) and \
         (mouse_coordinates[1] - self.y <= self.b):
 
             SCORE = SCORE + self.value
@@ -235,13 +248,17 @@ def bounce_wall(enemy):
 	enemy.bounce_wall()
 	
 def move(enemy):
+    """
+changes x,y by v_x and v_y - distances travelled in unit time
+    """
     enemy.x = enemy.x + enemy.v_x
     enemy.y = enemy.y + enemy.v_y
 
 	
 SCORE = 0
-TIME = 0
-ENEMIES = []
+TIME = 0  # time from start of game
+ENEMIES = [] #  array which holds things you click
+MAX_ENEMIES = 10  #  maximum amount of enemies on screen
 
 
 pygame.display.update()
@@ -255,12 +272,12 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
             
-        if event.type == GENERATE_BALL and len(ENEMIES) != Ball().MAX_AMOUNT:
-            ENEMIES.append(Ball())
-                
-        if event.type == GENERATE_RECT and len(ENEMIES) != Rect().MAX_AMOUNT:
-            ENEMIES.append(Rect())
-                                             
+        if len(ENEMIES)!=MAX_ENEMIES:
+            if event.type == GENERATE_BALL:
+                ENEMIES.append(Ball())       
+            if event.type == GENERATE_RECT:
+                ENEMIES.append(Rect())
+                                                
         if event.type == pygame.MOUSEBUTTONDOWN:
             #  check if  inside the enemy hitbox and get plus score
             Mouse_coordinates = (pygame.mouse.get_pos())  
@@ -272,9 +289,7 @@ while not finished:
                                                                             
     for enemy in ENEMIES:
         bounce_wall(enemy)
-    for enemy in ENEMIES:
         move(enemy)
-    for enemy in ENEMIES:
         create(enemy)
 
     SCORESIGN = bytes('Score:' + str(SCORE), encoding = 'utf-8')   
@@ -282,7 +297,10 @@ while not finished:
     screen.blit(img, (20, 20))
                                                                   
     pygame.display.update()
-    screen.fill(BLACK)
+    if not blank_name or SCORE < 10:
+        screen.fill(BLACK)
+    else:
+        screen.blit(ANON_surf, (0,0))
 
 pygame.quit()
 
@@ -297,3 +315,5 @@ for i in range (len(ScoreNames)):
     print(i + 1, ScoreNames[i][1], ScoreNames[i][0], ScoreNames[i][2],
           file  =  BESTPLAYERS)
 BESTPLAYERS.close()
+
+
