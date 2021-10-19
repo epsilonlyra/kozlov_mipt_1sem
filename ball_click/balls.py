1da66bccad58c09f756e5047095048660cd0f767
epsilonlyra <kozlov.de@phystech.edu>
2021-10-19 19:52:39 +0300

    added new enemy with dif mechanics

--- ball_click/balls.py
+++ ball_click/balls.py
@@ -1,20 +1,18 @@
 import pygame 
 from pygame.draw import *
-import random as sus
+import random 
 from random import randint
 
+
 def sign(x):
-  
   """
-if x>=0 returns 1
-else returns -1
+  if x>=0 returns 1
+  else returns -1
   """
-  
-  if (x>=0):
+  if (x >= 0):
     return(1)
   else:
     return(-1)
-
 
 
 #  if leaderboard file  doesnt exist, creates it
@@ -85,9 +83,9 @@ width = 1200
 height = 900
 screen = pygame.display.set_mode((width, height))
 
-font = pygame.font.SysFont(None, 24)
+font = pygame.font.SysFont(None, 24, False, True, None)
 
-ANON_surf= pygame.image.load('anon.bmp')
+ANON_surf= pygame.image.load('pictures/anon.bmp')  #  background incognito
 ANON_surf =  body_surf = pygame.transform.scale(ANON_surf, (width, height))
 
 RED = (255, 0, 0)
@@ -98,26 +96,27 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, WHITE]
 
        
 class Ball():
-     
+    """
+    Colorfull Ball, random bounce
+    """
     def __init__(self):
       
         self.GEN_RATE = 1000  #  time in ms beetwen ball generations
         self.color = COLORS[randint(0, 5)]
-        self.value = 1
-        self.speed = 10  # abs of  maxmovement unit tine in x or y direction
+        self.value = 1  #  worth in points
+        self.speed = 10  # abs of maxmovement in unit time in x/y direction
         self.r = randint(30, 50)
         
         self.x = randint(100, width - 100)
         self.y = randint(100, height - 100)
-        self.r = randint(30, 50)
         self.v_x = randint(-self.speed, self.speed)
         self.v_y = randint(-self.speed, self.speed)
-        
-        
+           
     def create(self):
         """
 	draws a ball on screen
@@ -132,12 +131,12 @@ class Ball():
             #  checking if on the next step ball touches left-right border
         if (self.x + self.v_x + self.r >= width) or \
                 (self.x  + self.v_x - self.r <= 0):     
-            k = randint(1,self.speed)  
+            k = randint(1, self.speed)  
             self.v_x = -sign(self.v_x) * k  #  bounce away with x_speed!=0
         
             #  change v_y
             self.v_y = randint(-self.speed, self.speed)
-        
+  
             #  checking if on the next step ball touches up -down border        
         if (self.y + self.r + self.v_y >= height) or \
                     (self.y - self.r + self.v_y <= 0):
@@ -151,33 +150,34 @@ class Ball():
 
     def check_click(self, Mouse_coordinates, SCORE):
         """
-  checks if mouse in hitbox, and if hit adds value to SCORE
-  returns list
-  list [0] = 1 if hit, 0 if not
-  list[1] - SCORE after click
-      """
+        checks if mouse in hitbox,
+        1 if hit, 0 if not
+        """
         if ((Mouse_coordinates[0] - self.x) **2 \
             + (Mouse_coordinates[1] - self.y) **2) \
                     <= (self.r) **2:
-            SCORE = SCORE + self.value
-            return(1,SCORE)
+            return(1)
         else:
-            return(0,SCORE)
-
+            return(0)
 
 GENERATE_BALL = pygame.USEREVENT + 0
 pygame.time.set_timer(GENERATE_BALL, Ball().GEN_RATE)
-          
+
+        
 class Rect():
+    """
+    Colorfull Rectangle(height>width),
+    bounces from whals randomly
+    """
      
     def __init__(self):
       
-        self.GEN_RATE = 1700 #  time in ms beetwen sqr generations
+        self.GEN_RATE = 1700 #  time in ms beetwen rect generations
         self.color = COLORS[randint(0, 5)]
-        self.value = 1
+        self.value = 1.5  #  worth in points
         self.speed = 5 # abs of movement in unit time in x or y direction
         self.a = randint(60, 100)  # rect width
-        self.b = randint(60 ,100)  #  rect height
+        self.b = randint(self.a + 10, 120)  #  rect height
         
         self.x = randint(100, width - 100)
         self.y = randint(100, height - 100)
@@ -218,24 +218,108 @@ class Rect():
 
     def check_click(self, mouse_coordinates, SCORE):
         """
-  checks if mouse in hitbox, and if hit adds value to SCORE
-  returns list
-  list [0] = 1 if hit, 0 if not
-  list[1] - SCORE after click
+        checks if mouse in hitbox, and if hit adds value to SCORE
+        1 if hit, 0 if not
         """
         if (mouse_coordinates[0] - self.x >=0) and \
-        (mouse_coordinates[0] - self.x <= self.a) and \
-        (mouse_coordinates[1] - self.y >= 0) and \
-        (mouse_coordinates[1] - self.y <= self.b):
-
-            SCORE = SCORE + self.value
-            return(1, SCORE)
+           (mouse_coordinates[0] - self.x <= self.a) and \
+           (mouse_coordinates[1] - self.y >= 0) and \
+           (mouse_coordinates[1] - self.y <= self.b):
+               return(1)
         else:
-            return(0,SCORE)   
+            return(0)
 
-
-GENERATE_RECT = pygame.USEREVENT + 1
+GENERATE_RECT= pygame.USEREVENT + 1
 pygame.time.set_timer(GENERATE_RECT, Rect().GEN_RATE)
+
+
+class Susqr():
+    """
+    Red Square, bounces from walls normally
+    if mouse to close tries to run away
+    if mouse and border are close teleports in the center
+    """
+    def __init__(self):
+      
+        self.GEN_RATE = 5000 #  time in ms beetwen sqr generations
+        self.color = RED
+        self.value = 2  #  worth in points
+        self.speed = 10  # abs of movement in unit time in x or y direction
+        self.b = randint(50, 100)  #  square length
+        self.mindist = 100  # from what dist. from center run away stars
+        
+        self.x = randint(100, width - 100)
+        self.y = randint(100, height - 100)
+        self.v_x = randint(-self.speed, self.speed)
+        self.v_y = randint(-self.speed, self.speed)
+        
+    def create(self):
+        """
+	draws a square on screen
+        """
+        rect(screen, self.color, (self.x, self.y, self.b, self.b))
+        
+    def run_away(self, Mouse_coordinates):
+        """
+        if mouse closer than self.mindist
+        speed of center  is away from the cursor;
+        if in this situation touches border
+        teleports to the center
+        """       
+        x_distance = (self.x - Mouse_coordinates[0] + self.b/2)
+        y_distance = (self.y - Mouse_coordinates[1] +  self.b/2)
+        distance = (x_distance **2 + y_distance **2) **(1/2)
+        if (distance <= self.mindist):
+             #  checking if on the next step rect touches left-right border
+            if (self.x + self.v_x + self.b >= width) or \
+               (self.x + self.v_x <= 0) or \
+               (self.y + self.v_y + self.b >=  height) or \
+               (self.y + self.v_y <= 0):
+                  #  teleport
+                  self.x = round(width / 2)
+                  self.y = round(height / 2)
+            try:
+                cos = (x_distance / distance)
+                sin = (y_distance / distance)
+            except:
+                cos = 0 
+                sin = 1
+            v = (self.v_x **2 + self.v_y **2) **(1/2)
+            self.v_x = round(cos * v)
+            self.v_y = round(sin * v)
+           
+    def bounce_wall(self):
+        """
+	checking if the ball reaches border in the next time moment
+	change velocity as  in elastic collisions
+        """
+            #  checking if on the next step rect touches left-right border
+        if (self.x + self.v_x + self.b >= width) or \
+           (self.x  + self.v_x <= 0):      
+               self.v_x = -(self.v_x)   #  bounce away with same speed
+        
+            #  checking if on the next step ball touches up-down border        
+        if (self.y + self.v_y + self.b >=  height) or \
+           (self.y + self.v_y <= 0):
+               self.v_y = -(self.v_y) #  bounce away with same speed
+
+
+    def check_click(self, mouse_coordinates, SCORE):
+        """
+        checks if mouse in hitbox, and if hit adds value to SCORE
+        1 if hit, 0 if not
+        """
+        if (mouse_coordinates[0] - self.x >=0) and \
+           (mouse_coordinates[0] - self.x <= self.b) and \
+           (mouse_coordinates[1] - self.y >= 0) and \
+           (mouse_coordinates[1] - self.y <= self.b):
+               return(1)
+        else:
+            return(0)
+
+GENERATE_SUSQR = pygame.USEREVENT + 2
+pygame.time.set_timer(GENERATE_SUSQR, Susqr().GEN_RATE)
+
 
 
 def check_click(enemy, mouse_coordinates, SCORE):
@@ -245,20 +329,30 @@ def create(enemy):
     enemy.create()
     
 def bounce_wall(enemy):
-	enemy.bounce_wall()
+    enemy.bounce_wall()
 	
 def move(enemy):
     """
-changes x,y by v_x and v_y - distances travelled in unit time
+    changes x,y by v_x and v_y - distances travelled in unit time
     """
     enemy.x = enemy.x + enemy.v_x
     enemy.y = enemy.y + enemy.v_y
 
+#  unused feature
+'''def accelerate(enemy):
+    """
+changes v_x,v_y by v_x and v_y - change in velocity
+    """
+    enemy.v_x = enemy.v_x + enemy.g_x
+    enemy.v_y = enemy.v_y + enemy.g_y'''
+
+def run_away(enemy, Mouse_coordinates):
+    enemy.run_away(Mouse_coordinates)
 	
 SCORE = 0
 TIME = 0  # time from start of game
-ENEMIES = [] #  array which holds things you click
-MAX_ENEMIES = 10  #  maximum amount of enemies on screen
+ENEMIES = [] #  array which holds things you can click on
+MAX_ENEMIES = 5  #  maximum amount of enemies on  the screen
 
 
 pygame.display.update()
@@ -267,7 +361,8 @@ finished = False
 
 while not finished:
     clock.tick(FPS)
-    TIME += clock.get_time()   
+    TIME += clock.get_time()
+    Mouse_coordinates = (pygame.mouse.get_pos()) 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
@@ -277,21 +372,27 @@ while not finished:
                 ENEMIES.append(Ball())       
             if event.type == GENERATE_RECT:
                 ENEMIES.append(Rect())
-                                                
+            if event.type == GENERATE_SUSQR:
+                ENEMIES.append(Susqr())                                   
         if event.type == pygame.MOUSEBUTTONDOWN:
             #  check if  inside the enemy hitbox and get plus score
-            Mouse_coordinates = (pygame.mouse.get_pos())  
-            for enemy in ENEMIES: 
-                if check_click(enemy, Mouse_coordinates, SCORE)[0] == 1:
+            for enemy in ENEMIES:
+                if check_click(enemy, Mouse_coordinates, SCORE) == 1:
                     ENEMIES.remove(enemy)
-                    SCORE = check_click(enemy, Mouse_coordinates, SCORE)[1]
-                        
-                                                                            
+                    SCORE = SCORE + enemy.value
+                                                                                                            
     for enemy in ENEMIES:
+        try:
+            run_away(enemy, Mouse_coordinates)
+        except:
+            (None)  
+        '''accelerate(enemy)'''  #  unused feature
         bounce_wall(enemy)
         move(enemy)
         create(enemy)
+        
 
+    #  using SCORESIGN to render pygame font
     SCORESIGN = bytes('Score:' + str(SCORE), encoding = 'utf-8')   
     img = font.render(SCORESIGN, True, WHITE)
     screen.blit(img, (20, 20))
@@ -299,9 +400,9 @@ while not finished:
     pygame.display.update()
     if not blank_name or SCORE < 10:
         screen.fill(BLACK)
-    else:
+    else:  #  enter anonimus mode
         screen.blit(ANON_surf, (0,0))
-
+        
 pygame.quit()
 
 
